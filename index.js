636d1500afe0d29c1e2bf83852a9a4e13e52e321
Nicolas Campos <nicolascampos1621@gmail.com>
2025-05-19 19:38:31 -0500

    definir 3 rutas para desplegar urls en el navegador

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 // paso 1: importamos las dependendias (librerias, paquetes)
-// const express = require( 'express' );                            //commonJS
+// const express = require( 'express' );               //importacion de dependencia usando commonJS
 import express from 'express';             //ESModule
 
 // paso 2: ejecutamos express
 const app = express();
 
-//paso 3:lanzamos el servidor web usando express escuchando en el puerto 300
+//paso 3: crear los Endpoins (putos de entrada)
+app.get( '/', ( req, res ) => {
+    res.send( '<h1>Home</h1>');
+});
+//http://localhost:<port>
+app.get( '/abaut-us', (req, res) => {
+    res.send( '<h1>Abaut us</h1>');
+});
+app.get('/contact', (req, res) =>{
+    res.send('h1<contact</h1>');
+});
+
+
+//paso 4:lanzamos el servidor web usando express escuchando en el puerto 300
+//         http://localhost:<port>
 app.listen( 3000, () =>{
     console.log( 'servidor lanzado exitosamente')
 });
