d24f48c21778996fae481d40023d2412420c29a3
Diego Munoz <diegoamunozb94@gmail.com>
2025-05-29 01:05:35 -0500

    se crearon los endpoints restantes para la nueva entidad de reservas

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,7 @@
 // paso 1: importamos las dependendias (librerias, paquetes)
 // const express = require( 'express' );               //importacion de dependencia usando commonJS
 import express from 'express';
-import products from './routes/product.route.mjs';
+import reservas from './routes/reservas.route.mjs'
 import dbconnect from './config/mongo.config.mjs';    ///importamos la conexion a al base de datos 
 
 const app = express();
@@ -9,7 +9,10 @@ const app = express();
 //invocar la configuracion de la conexion a la base de datos
 dbconnect();
 
-app.use(products);
+// app.use(products);
+
+app.use(express.json());
+app.use( reservas);
 
 app.listen( 3000, () =>{
     console.log( 'servidor corriendo en http://localhost:3000')
