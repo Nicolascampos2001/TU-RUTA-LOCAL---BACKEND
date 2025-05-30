8976a07425a0742855ec97ca94891a96cf8ae0e2
AGr <gonzalezrios205@gmail.com>
2025-05-29 19:50:59 -0500

    creacion route en auth

--- src/routes/auth.router.mjs
+++ src/routes/auth.router.mjs
@@ -1,7 +1,9 @@
 import express from 'express'
+import { createUser } from '../controllers/users.controllers.mjs';
 
 const router = express.Router();
 
 //define las rutas para la entidad Auth
+router.post('/api/register',createUser)
 
 export default router;
